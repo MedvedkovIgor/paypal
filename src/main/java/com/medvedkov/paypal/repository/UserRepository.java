3e9b247df8609a71c4b3c75094f1906882dd729c
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-17 11:47:47 +0200

    annotated as repository

--- src/main/java/com/medvedkov/paypal/repository/UserRepository.java
+++ src/main/java/com/medvedkov/paypal/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.medvedkov.paypal.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.medvedkov.paypal.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
