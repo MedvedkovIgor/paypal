4085dc81136e5b9def08bf7852fb17ab885ff197
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-22 17:45:25 +0200

    added redirection on login.html

--- src/main/java/com/medvedkov/paypal/controller/LoginController.java
+++ src/main/java/com/medvedkov/paypal/controller/LoginController.java
@@ -1,21 +1,26 @@
 package com.medvedkov.paypal.controller;
 
-import com.medvedkov.paypal.entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class LoginController {
 
-    @PostMapping(value = "/login/local")
-    public void loginViaResourceAccount(User user){
-        System.out.println(user);
+    @RequestMapping(value = "/signin")
+    public String signin() {
+        return "login.html";
     }
 
-    @RequestMapping(value = "/google")
-    public void loginViaGoogle() {
+//    @PostMapping(value = "/local")
+//    public void loginViaResourceAccount(@RequestBody String userName, @RequestBody String password){
+//        SecurityContextHolder.getContext().setAuthentication(
+//                new UsernamePasswordAuthenticationToken(userName,
+//                        null, /*Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"))*/null));
+//    }
 
-    }
+//    @RequestMapping(value = "/google")
+//    public void loginViaGoogle() {
+//
+//    }
 }
