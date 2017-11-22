ceb2e08ecae65068ff127d15ed9c534ac1a6cf69
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-22 12:39:25 +0200

    removed unnecessary getters/setters

--- src/main/java/com/medvedkov/paypal/entity/User.java
+++ src/main/java/com/medvedkov/paypal/entity/User.java
@@ -35,28 +35,4 @@ public class User {
         this.roles = roles;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<UserRole> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<UserRole> roles) {
-        this.roles = roles;
-    }
-
 }
