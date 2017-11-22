ae22a05e604053d652f685359eed5603e40e9625
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-22 17:46:56 +0200

    simplified authentication

--- src/main/java/com/medvedkov/paypal/security/SignInAdapterImpl.java
+++ src/main/java/com/medvedkov/paypal/security/SignInAdapterImpl.java
@@ -1,11 +1,9 @@
 package com.medvedkov.paypal.security;
 
-import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
@@ -19,10 +17,10 @@ public class SignInAdapterImpl implements SignInAdapter {
 
     @Override
     public String signIn(String localUserId, Connection<?> connection, NativeWebRequest request) {
-        logger.info("Sign in as "+connection.getDisplayName());
+        logger.info("Sign in as " + connection.getDisplayName());
         SecurityContextHolder.getContext().setAuthentication(
                 new UsernamePasswordAuthenticationToken(connection.getDisplayName(),
-                        null, Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"))));
+                        null, /*Arrays.asList(new SimpleGrantedAuthority("FACEBOOK_USER"))*/null));
         return null;
     }
 }
