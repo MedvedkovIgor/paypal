c200981723e5b38332d5c5f035fabf8dea073a86
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-22 17:48:22 +0200

    removed preauthorize for user

--- src/main/java/com/medvedkov/paypal/controller/PayPalController.java
+++ src/main/java/com/medvedkov/paypal/controller/PayPalController.java
@@ -10,7 +10,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/payment")
-@PreAuthorize("hasAuthority('STANDARD_USER')")
+//@PreAuthorize("hasAuthority('STANDARD_USER')")
 @ConfigurationProperties(prefix = "security.paypal")
 public class PayPalController {
 
