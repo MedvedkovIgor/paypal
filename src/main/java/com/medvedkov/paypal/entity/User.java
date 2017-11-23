12a02a3dcae985f32444567168a2368d11609e83
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-23 12:50:30 +0200

    added lombok annotations

--- src/main/java/com/medvedkov/paypal/entity/User.java
+++ src/main/java/com/medvedkov/paypal/entity/User.java
@@ -1,33 +1,24 @@
 package com.medvedkov.paypal.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "USERS")
+@Data
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Getter
-    @Setter
     private String username;
-
-    @Getter
-    @Setter
     private String password;
-
-    @Getter
-    @Setter
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<UserRole> roles;
-
-    public User() {
-    }
 
     public User(String username, String password, List<UserRole> roles) {
         this.username = username;
--- src/main/java/com/medvedkov/paypal/entity/UserRole.java
+++ src/main/java/com/medvedkov/paypal/entity/UserRole.java
@@ -1,23 +1,19 @@
 package com.medvedkov.paypal.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "USER_ROLES")
+@Data
+@NoArgsConstructor
 public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Getter
-    @Setter
     private String name;
-
-    UserRole() {
-    }
 
     public UserRole(String name) {
         this.name = name;
