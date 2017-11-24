7345d79246f0ffa215d545e3650f73594fa8b9b9
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-24 11:16:45 +0200

    replaced getters/setters on data

--- src/main/java/com/medvedkov/paypal/entity/UserRole.java
+++ src/main/java/com/medvedkov/paypal/entity/UserRole.java
@@ -1,19 +1,23 @@
 package com.medvedkov.paypal.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "USER_ROLES")
-@Data
-@NoArgsConstructor
 public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Getter
+    @Setter
     private String name;
+
+    UserRole() {
+    }
 
     public UserRole(String name) {
         this.name = name;
