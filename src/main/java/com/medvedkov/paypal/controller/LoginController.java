6faad0574c07acd298c7684595664381f381ec8e
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-24 11:19:29 +0200

    refactored

--- src/main/java/com/medvedkov/paypal/controller/LoginController.java
+++ src/main/java/com/medvedkov/paypal/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.medvedkov.paypal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //@RestController
@@ -11,6 +12,11 @@ public class LoginController {
     public String signin() {
         return "login.html";
     }
+
+//    @GetMapping("/")
+//    public String root() {
+//        return "index.html";
+//    }
 
 //    @PostMapping(value = "/local")
 //    public void loginViaResourceAccount(@RequestBody String userName, @RequestBody String password){
--- src/main/java/com/medvedkov/paypal/security/SignInAdapterImpl.java
+++ src/main/java/com/medvedkov/paypal/security/SignInAdapterImpl.java
@@ -3,7 +3,6 @@ package com.medvedkov.paypal.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.SignInAdapter;
