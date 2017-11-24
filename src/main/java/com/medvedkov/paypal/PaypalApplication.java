e4ecee6b5b13c08d60887c34d7847e391aaa5124
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-24 11:20:06 +0200

    changed to real project config

--- src/main/java/com/medvedkov/paypal/PaypalApplication.java
+++ src/main/java/com/medvedkov/paypal/PaypalApplication.java
@@ -2,12 +2,20 @@ package com.medvedkov.paypal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class PaypalApplication {
+@EnableScheduling
+public class PaypalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaypalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PaypalApplication.class);
+    }
 }
