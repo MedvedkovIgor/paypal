53d7394c8cfc2bd91ad8d98489bcf56db7c8af0e
MedvedkovIgor <medvedkovigor1995@gmail.com>
2017-11-24 11:17:25 +0200

    changed requestmapping

--- src/main/java/com/medvedkov/paypal/controller/RecourceController.java
+++ src/main/java/com/medvedkov/paypal/controller/RecourceController.java
@@ -3,17 +3,21 @@ package com.medvedkov.paypal.controller;
 import com.medvedkov.paypal.entity.User;
 import com.medvedkov.paypal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/resources")
+@RequestMapping(value = "/signin/resources")
 public class RecourceController {
     @Autowired
     private UserRepository userRepository;
 
     @RequestMapping(value = "/users")
-    public Iterable<User> getAllUsers(){
+    public Iterable<User> getAllUsers() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return userRepository.findAll();
     }
 }
