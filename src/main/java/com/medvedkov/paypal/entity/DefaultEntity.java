0e9d7777610d3d2654228a4629e06a47b88a09ad
Igor Medvedkov <medvedkovigor1995@gmail.com>
2017-11-26 10:02:57 +0200

    marked as @data and @serializable

--- src/main/java/com/medvedkov/paypal/entity/DefaultEntity.java
+++ src/main/java/com/medvedkov/paypal/entity/DefaultEntity.java
@@ -1,10 +1,14 @@
 package com.medvedkov.paypal.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "DEFAULT_ENTITY")
-public class DefaultEntity {
+@Data
+public class DefaultEntity implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
